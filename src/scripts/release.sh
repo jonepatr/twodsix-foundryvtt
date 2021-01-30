6eb609dc35eaa0158e3c81a107dacd5d757ea361
Patrik Jonell <pjjonell@kth.se>
2021-01-30 01:25:37 +0100

    chore: hopefully last time

--- src/scripts/release.sh
+++ src/scripts/release.sh
@@ -1,5 +1,9 @@
+SEARCH_PATTERN='(\s*"(manifest|download)"\:).*'
+DEVELOPMENT_REPLACE="\1 \"https://github.com/xdy/twodsix-foundryvtt/releases/download/v$1/system.json\","
+MASTER_REPLACE="\1 \"https://github.com/xdy/twodsix-foundryvtt/releases/latest/system.json\","
+
 sed -i -e 's|\(.*"version"\): "\(.*\)",.*|\1: '"\"$1\",|" static/system.json &&
-  if echo "$1" | grep -q "development"; then sed -i -e  s"/\"\(download\|manifest\)\":.*/\"\1\": \"https:\/\/github.com\/xdy\/twodsix-foundryvtt\/releases\/download\/v$1\/system.json\",/" static/system.json; else sed -i -e  s"/\"\(download\|manifest\)\":.*/\"\1\": \"https:\/\/github.com\/xdy\/twodsix-foundryvtt\/releases\/download\/v$1\/system.json\",/" static/system.json; fi &&
+  if echo "$1" | grep -q "development"; then sed -i -e -E  s"~$SEARCH_PATTERN~$DEVELOPMENT_REPLACE~" static/system.json; else sed -i -e -E  s"~$SEARCH_PATTERN~$MASTER_REPLACE~" static/system.json; fi &&
   cp static/system.json dist &&
   sed -i -e 's|\(.*"version"\): "\(.*\)",.*|\1: '"\"$1\",|" package.json &&
   npm install &&
